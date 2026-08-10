fbc7d07acf4da06c68146c8384c432a19f3071fb
maryam-2021 <maryam.zafar1983@gmail.com>
2026-08-10 04:04:28 +0100

    Wire new authority routes and localized hubs

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,16 +7,27 @@ import PrefixConverterPage from '@/pages/PrefixConverterPage';
 import ChartsPage from '@/pages/ChartsPage';
 import GuidesIndexPage from '@/pages/GuidesIndexPage';
 import NotFoundPage from '@/pages/NotFoundPage';
+import AboutPage from '@/pages/AboutPage';
+import ContactPage from '@/pages/ContactPage';
+import PrivacyPage from '@/pages/PrivacyPage';
+import TermsPage from '@/pages/TermsPage';
+import CategoryHubPage from '@/pages/CategoryHubPage';
+import LocalizedHubPage from '@/pages/LocalizedHubPage';
+import { LOCALIZED_HUBS } from '@/data/localized-hubs';
 import NanoVsMicro from '@/pages/articles/NanoVsMicro';
 import SiPrefixesExplained from '@/pages/articles/SiPrefixesExplained';
 import NmToUmGuide from '@/pages/articles/NmToUmGuide';
 import CapacitorCodes from '@/pages/articles/CapacitorCodes';
 import McgVsUgVsMg from '@/pages/articles/McgVsUgVsMg';
 import LatencyChart from '@/pages/articles/LatencyChart';
+import NewScienceGuide from '@/pages/articles/NewScienceGuide';
 import NanoToMicroHubPage from '@/pages/NanoToMicroHubPage';
 import NmToUmSpokePage from '@/pages/length/NmToUmSpokePage';
 import NsToUsSpokePage from '@/pages/time/NsToUsSpokePage';
 import NsvToUsvSpokePage from '@/pages/radiation/NsvToUsvSpokePage';
+import GrayVsSievertPage from '@/pages/radiation/GrayVsSievertPage';
+import UsvVsUsvPerHourPage from '@/pages/radiation/UsvVsUsvPerHourPage';
+import UsvToMsvPage from '@/pages/radiation/UsvToMsvPage';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -50,6 +61,11 @@ export default function App() {
         <Route path="/radiation/nsv-to-usv/" element={<NsvToUsvSpokePage />} />
         <Route path="/nanosieverts-to-microsieverts" element={<Navigate to="/radiation/nsv-to-usv/" replace />} />
         <Route path="/nanosieverts-to-microsieverts/" element={<Navigate to="/radiation/nsv-to-usv/" replace />} />
+        <Route path="/radiation/gray-vs-sievert" element={<GrayVsSievertPage />} />
+        <Route path="/radiation/usv-vs-usv-per-hour" element={<UsvVsUsvPerHourPage />} />
+        <Route path="/radiation/usv-to-msv" element={<UsvToMsvPage />} />
+
+        <Route path="/units/:hubSlug" element={<CategoryHubPage />} />
 
         <Route path="/si-prefix-converter" element={<PrefixConverterPage />} />
         <Route path="/conversion-charts" element={<ChartsPage />} />
@@ -60,6 +76,20 @@ export default function App() {
         <Route path="/guides/capacitor-codes-explained" element={<CapacitorCodes />} />
         <Route path="/guides/mcg-vs-ug-vs-mg" element={<McgVsUgVsMg />} />
         <Route path="/guides/ns-vs-us-ms-latency-chart" element={<LatencyChart />} />
+        <Route path="/guides/sievert-gray-becquerel-difference" element={<NewScienceGuide slug="sievert-gray-becquerel-difference" />} />
+        <Route path="/guides/visible-light-wavelength-nm-to-um" element={<NewScienceGuide slug="visible-light-wavelength-nm-to-um" />} />
+        <Route path="/guides/semiconductor-process-nodes-explained" element={<NewScienceGuide slug="semiconductor-process-nodes-explained" />} />
+        <Route path="/guides/cell-size-in-micrometers" element={<NewScienceGuide slug="cell-size-in-micrometers" />} />
+
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/terms" element={<TermsPage />} />
+
+        {LOCALIZED_HUBS.map((locale) => (
+          <Route key={locale.code} path={`/${locale.code}/`} element={<LocalizedHubPage code={locale.code} />} />
+        ))}
+
         <Route path="/:slug" element={<ConverterRoute />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
